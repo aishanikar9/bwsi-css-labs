768cb725e17d141c88a4428993879f04c2c5239e
aishanikar9 <aishanikar9@gmail.com>
2026-03-29 21:33:21 -0700

    Accepted Higher speed, merged conflict

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,11 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
-<<<<<<< HEAD
 This is to simulate a change made on a robot: robot_speed = 8 # m/s
-=======
-This is to simulate a change made on a robot: robot_speed = 3 # m/s
->>>>>>> 6e10818ead408cb6367f423cf38e333a22a1bce6
 """
 
 def main():
